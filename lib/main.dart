fb26752deafb17d01a5cb16de51b8074468441a0
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-28 17:26:58 -0600

    Se creo el inicio de la app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:qrscanner_app/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QRReader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage()
+      },
+    );
+  }
+}
