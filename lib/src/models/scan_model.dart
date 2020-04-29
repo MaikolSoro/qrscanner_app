4120e7668fa183bafb4941fd8660053a45c0cd73
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-29 15:50:27 -0600

    Crear el mapa

--- lib/src/models/scan_model.dart
+++ lib/src/models/scan_model.dart
@@ -1,3 +1,5 @@
+import 'package:latlong/latlong.dart';
+
 class ScanModel {
     
     int id;
@@ -28,4 +30,15 @@ class ScanModel {
         "tipo": tipo,
         "valor": valor,
     };
+
+     LatLng getLatLng() {
+      
+      // geo:40.724233047051705,-74.00731459101564
+      final lalo = valor.substring(4).split(',');
+      final lat  = double.parse( lalo[0] );
+      final lng  = double.parse( lalo[1] );
+
+      return LatLng( lat, lng );
+
+    }
 }
--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 import 'package:qrscanner_app/src/models/scan_model.dart';
- 
 
- class MapaPage extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
-     return Scaffold(
-        appBar: AppBar(
-          title: Text('Coordenadas QR'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.my_location),
-              onPressed: (){},              
-            )
-          ],
-        ),
-        body: Center(
-          child: Text( scan.valor ),
-        ),
-     );
-   }
- }
+class MapaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final ScanModel scan = ModalRoute.of(context).settings.arguments; 
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coordenadas QR'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.my_location),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: _crearFlutterMap(scan)
+    );
+  }
+
+  Widget _crearFlutterMap( ScanModel scan ) {
+
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10
+      ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+
+  }
+  _crearMapa() {
+
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1IjoibXJzdXJpIiwiYSI6ImNrOWx1YWtsYzAyOHEzaWp5NXBjdms1YncifQ.sK9NX50ETP5V2e3uxNlXMw',
+        'id': 'mapbox.streets'
+      }
+    );
+
+
+  }
+}
