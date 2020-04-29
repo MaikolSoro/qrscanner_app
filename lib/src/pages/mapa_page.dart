f497df30f0aaf4c77fed1121f100cbc52026145e
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-29 16:11:32 -0600

    Se creo el marcador para el mapa

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 import 'package:qrscanner_app/src/models/scan_model.dart';
 
 
 class MapaPage extends StatelessWidget {
+
+  final  map = new MapController();
   @override
   Widget build(BuildContext context) {
 
@@ -16,7 +17,9 @@ class MapaPage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: (){},
+            onPressed: (){
+              map.move(scan.getLatLng(), 15);
+            },
           )
         ],
       ),
@@ -27,12 +30,14 @@ class MapaPage extends StatelessWidget {
   Widget _crearFlutterMap( ScanModel scan ) {
 
     return FlutterMap(
+      mapController: map,
       options: MapOptions(
         center: scan.getLatLng(),
-        zoom: 10
+        zoom: 15
       ),
       layers: [
         _crearMapa(),
+        _crearMarcadores(scan)
       ],
     );
 
@@ -44,10 +49,32 @@ class MapaPage extends StatelessWidget {
       '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
         'accessToken': 'pk.eyJ1IjoibXJzdXJpIiwiYSI6ImNrOWx1YWtsYzAyOHEzaWp5NXBjdms1YncifQ.sK9NX50ETP5V2e3uxNlXMw',
-        'id': 'mapbox.streets'
+        'id': 'mapbox.satellite'
+        // streets, dark, light,outdoors, satellite // tipos de mapa
       }
     );
 
 
+  }
+
+// creo el marcador en el mapa
+  _crearMarcadores(ScanModel scan) {
+
+    return MarkerLayerOptions(
+      markers:<Marker>[
+        Marker(
+          width: 100.0,
+          height: 100.0,
+          point: scan.getLatLng(),
+          builder: ( context ) => Container(
+            child: Icon(
+              Icons.location_on,
+             size: 70.0, 
+             color: Theme.of(context).primaryColor
+             ),
+          )
+        )
+      ]
+    );
   }
 }
